00916deadea00c217529937018642f226b4dc4d2
Tacho2 <egbertcm23@gmail.com>
2026-02-20 12:17:16 -0500

    Hardcode nudge dependencies to avoid complex parsing errors

--- nudge/setup.py
+++ nudge/setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("../misc/README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-with open("../requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#") and "@" not in line]
-
 setup(
     name='nudge',
     version='0.5.0',
@@ -17,5 +11,12 @@ setup(
     url='tba',
     description='Neurally gUided Differentiable loGic policiEs (NUDGE)',
     long_description=long_description,
-    install_requires=requirements,
+    install_requires=[
+        "tyro",
+        "pygame",
+        "opencv-python",
+        "numpy",
+        "gymnasium",
+        "torch"
+    ],
 )
